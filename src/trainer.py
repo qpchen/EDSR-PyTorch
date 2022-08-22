c2368acde7d984922156fcbbb001fedc6a858eb5
qpchen <chenqiangpu@foxmail.com>
2022-08-23 00:57:02 +0800

    modify ignore for Starlight HPC env

--- src/trainer.py
+++ src/trainer.py
@@ -76,7 +76,7 @@ class Trainer():
                 loss_forw = self.loss(sr, hr)
                 loss_back = self.loss(br, lr)
                 loss = (loss_forw + loss_back + loss_fea1 + loss_fea2) / 4
-            elif self.args.model == 'BIAANV3F' or self.args.model == 'BIAANV9A':  # BIAAN means version 5
+            elif self.args.model == 'BIAANV3F' or self.args.model == 'BIAANV9A':
                 sr, br, ff1, ff2, bf2, bf3 = self.model(lr, 0, hr)
                 loss_fea1 = self.loss(ff1, bf3)/2
                 loss_fea2 = self.loss(ff2, bf2)/2
