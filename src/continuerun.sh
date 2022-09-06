00cb7f4607edc08bdd73c899819a7ef7d077c936
qpchen <chenqiangpu@foxmail.com>
2022-09-06 19:26:37 +0800

    add python environment to print output immediately

--- src/continuerun.sh
+++ src/continuerun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# set python output immediately
+export PYTHONUNBUFFERED=1
 export CUDA_VISIBLE_DEVICES=1
 nohup python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 1e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --gclip 5 --model BIAANV9B --reset --save biann_v9bg_x2  > train_v3g.out 2>&1 &
 export CUDA_VISIBLE_DEVICES=2
