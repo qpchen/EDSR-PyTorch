b8b456f530abd27332ea4e9f9db973d2622bc71c
qpchen <chenqiangpu@foxmail.com>
2022-09-14 11:28:35 +0800

    change yinhe run scripts

--- src/startrun.sh
+++ src/startrun.sh
@@ -10,4 +10,4 @@
 
 
 export CUDA_VISIBLE_DEVICES=0
-yhrun -n 1 -N 1-1 -p gpu_v100 python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --gclip 5 --model BIAANV9C --reset --save biann_v9cgl5_x2
+yhrun -n 1 -N 1-1 -p gpu_v100 python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --model BIAANV10 --reset --save biann_v10g_x2
