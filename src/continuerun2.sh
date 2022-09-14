7d088e21642acdea0f089d1c30afc3a5fa5b8230
qpchen <chenqiangpu@foxmail.com>
2022-09-14 10:38:22 +0800

    add no share trunk models run scripts as on the second compute node

---
+++ src/continuerun2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# set python output immediately
+export PYTHONUNBUFFERED=1
+
+
+export CUDA_VISIBLE_DEVICES=1
+nohup python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --model BIAANV9A --reset --save biann_nov9a_x2 > train_nov9a.out 2>&1 &
+export CUDA_VISIBLE_DEVICES=2
+nohup python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --model BIAANV9B --reset --save biann_nov9b_x2 > train_nov9b.out 2>&1 &
+export CUDA_VISIBLE_DEVICES=3
+nohup python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --model BIAANV9C --reset --save biann_nov9c_x2 > train_nov9c.out 2>&1 &
+
+#####################################################################
+# can use '--jobid=id' to continue yhbatch/yhrun jobs to old queue
+#####################################################################
---
+++ src/startrun2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#########################################
+# run this script use command following:
+# yhbatch -N 1-1 -n 1 -p gpu_v100 startrun.sh
+#########################################
+
+#export CUDA_VISIBLE_DEVICES=2,3
+#yhrun -n 1 -N 1-1 -p gpu_v100 python main.py --n_GPUs 2 --model AAN --scale 4 --patch_size 256 --batch_size 32  --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --reset --save ann_x4
+
+
+export CUDA_VISIBLE_DEVICES=0
+yhrun -n 1 -N 1-1 -p gpu_v100 python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --model BIAANV9 --reset --save biann_nov9_x2
