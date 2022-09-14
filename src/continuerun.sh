2bd148b27de1c4357a343fecc90e8eab48a89f3e
qpchen <chenqiangpu@foxmail.com>
2022-09-14 10:01:52 +0800

    modify some training script to train on YinHe

--- src/continuerun.sh
+++ src/continuerun.sh
@@ -12,3 +12,7 @@ export CUDA_VISIBLE_DEVICES=2
 nohup python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --gclip 5 --model BIAANV9B --load biann_v9bg_x2 --resume -1 > train_v9bg.out 2>&1 &
 export CUDA_VISIBLE_DEVICES=3
 nohup python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --gclip 5 --model BIAANV3D --reset --save biann_v3dg_x2  > train_v3dg.out 2>&1 &
+
+#####################################################################
+# can use '--jobid=id' to continue yhbatch/yhrun jobs to old queue
+#####################################################################
