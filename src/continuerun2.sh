a757aba61bc927cf18d543da013fab5f622f8c12
qpchen <chenqiangpu@foxmail.com>
2022-09-14 12:53:58 +0800

    change yinhe run scripts

--- src/continuerun2.sh
+++ src/continuerun2.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#startid=999
-read -rp "Enter exist jobid, please: " startid
-
 # set python output immediately
 export PYTHONUNBUFFERED=1
 
@@ -18,6 +15,8 @@ nohup python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 20
 #####################################################################
 
 # cannot use yhbatch to run this script, may need some help
+#startid=999
+#read -rp "Enter exist jobid, please: " startid
 #export CUDA_VISIBLE_DEVICES=1
 #yhrun -n 1 -N 1-1 -p gpu_v100 --jobid=$startid python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --model BIAANV9A --reset --save biann_nov9a_x2
 #export CUDA_VISIBLE_DEVICES=2
