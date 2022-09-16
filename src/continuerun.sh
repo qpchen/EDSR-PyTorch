6e76dcd59f9a36bd3782c2c0c963f21b9e5908b2
qpchen <chenqiangpu@foxmail.com>
2022-09-16 09:40:07 +0800

    change training model by yinhe run scripts

--- src/continuerun.sh
+++ src/continuerun.sh
@@ -11,7 +11,7 @@ nohup python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 20
 export CUDA_VISIBLE_DEVICES=2
 nohup python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --gclip 5 --model BIAANV12 --reset --save biann_v12_x2 > train_v12.out 2>&1 &
 export CUDA_VISIBLE_DEVICES=3
-nohup python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --gclip 5 --model BIAANV9C -load biann_nov9cg_x2 --resume -1 > train_nov9cg.out 2>&1 &
+nohup python main.py --n_GPUs 1 --scale 2 --patch_size 128 --lr 5e-4 --epochs 2000 --skip_threshold 1e6 --data_test Set5 --batch_size 32 --gclip 5 --model BIAANV9C --load biann_nov9cg_x2 --resume -1 > train_nov9cg.out 2>&1 &
 
 #####################################################################
 # can use '--jobid=id' to continue yhbatch/yhrun jobs to old queue
