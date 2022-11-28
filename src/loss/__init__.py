839243256d36e4feb24c57f474774dc941dfbc36
qpchen <chenqiangpu@foxmail.com>
2022-11-28 21:28:09 +0800

    add Smooth L1 loss

--- src/loss/__init__.py
+++ src/loss/__init__.py
@@ -25,6 +25,8 @@ class Loss(nn.modules.loss._Loss):
                 loss_function = nn.MSELoss()
             elif loss_type == 'L1':
                 loss_function = nn.L1Loss()
+            elif loss_type == 'SmoothL1':
+                loss_function = nn.SmoothL1Loss()
             elif loss_type.find('VGG') >= 0:
                 module = import_module('loss.vgg')
                 loss_function = getattr(module, 'VGG')(
