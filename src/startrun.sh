52b9f9812cdd29ad9b7859f0eeea6bea8337a7fc
qpchen <chenqiangpu@foxmail.com>
2023-02-22 21:36:02 +0800

    modify args to the same as ConvNeXt

--- src/startrun.sh
+++ src/startrun.sh
@@ -16,4 +16,8 @@
 
 #python main.py --n_GPUs 1 --scale 2 --patch_size 128 --batch_size 32 --data_test Set5 --data_range 1-900 --loss 1*MSE --lr 1e-3 --n_colors 1 --optimizer ADAM --skip_threshold 1e6 --epochs 3000 --model BIFSRCNNLIV4 --reset --save bifsrcnnLI_v4_x2
 
-yhrun -n 1 -N 1-1 -p gpu_v100 python main.py --n_GPUs 4 --scale 2 --patch_size 128 --batch_size 32 --data_test Set5 --loss 1*SmoothL1 --lr 1e-3 --n_colors 1 --optimizer ADAM --skip_threshold 1e6 --epochs 3000 --depths 3+3+27+3 --dims 128+256+512+1024 --model SRARNV2 --save ../srarn/srarn_v2e7_x2 --reset
+# yhrun -n 1 -N 1-1 -p gpu_v100 python main.py --n_GPUs 4 --scale 2 --patch_size 128 --batch_size 32 --data_test Set5 --loss 1*SmoothL1 --lr 1e-3 --n_colors 1 --optimizer ADAM --skip_threshold 1e6 --epochs 3000 --depths 3+3+27+3 --dims 128+256+512+1024 --model SRARNV2 --save ../srarn/srarn_v2e7_x2 --reset
+
+yhrun -n 1 -N 1-1 -p gpu_v100 python main.py --n_GPUs 2 --scale 2 --patch_size 128 --batch_size 32 --skip_threshold 1e6 --epochs 3000 --data_test Set5+Set14 --loss 1*SmoothL1 --lr 4e-3 --n_colors 1 --optimizer AdamW --weight_decay 0.05 --depths 3+3+9+3 --dims 48+96+192+384 --model SRARNV2 --save ../srarn/srarn_v2d5_div_opt_x2 --reset
+
+yhrun -n 1 -N 1-1 -p gpu_v100 python main.py --n_GPUs 2 --scale 2 --patch_size 128 --batch_size 32 --skip_threshold 1e6 --epochs 3000 --data_train DF2K --data_range 1-3550 --data_test Set5+Set14 --loss 1*SmoothL1 --lr 4e-3 --n_colors 1 --optimizer AdamW --weight_decay 0.05 --depths 3+3+9+3 --dims 48+96+192+384 --model SRARNV2 --save ../srarn/srarn_v2d5_df_opt_x2 --reset
