a2a05e592c3790ca33872f9c7dd30f0197651615
qpchen <chenqiangpu@foxmail.com>
2023-03-07 22:12:52 +0800

    temp fix multi GPU training error for thop by skip

--- src/main.py
+++ src/main.py
@@ -71,17 +71,19 @@ def main():
                             col_names = ("output_size", "num_params", "params_percent", "mult_adds"),
                             depth = 6, 
                             mode = _mode, 
-                            row_settings = ("depth", "var_names"))))
+                            row_settings = ("depth", "var_names"),
+                            verbose=0)))
                 # checkpoint.write_log(str(summary(_model, [(_batch_size, args.n_colors, 1280, 720), args.scale])))
                 ################# get model info from thop #####################
-                # _input = torch.randint(0, 255, _input_size, device=_device)
-                _input = torch.randn(_input_size, device=_device)
-                _macs, _params = profile(_model, inputs=(_input, args.scale))
-                _m_fm, _p_fm = clever_format([_macs, _params], "%.3f")#inputs=(_input, args.scale))
-                _trop = str('\nThop: %s\'s total Parameters: %s(%d); MACs(Multi-Adds): %s(%d).\n' % (args.model, _p_fm, _params, _m_fm, _macs))
-                # print(_trop)
-                checkpoint.write_log(_trop)
-                _input = _input.to(torch.device('cpu'))
+                if args.n_GPUs == 1:  # TODO: fix the thop error when use data_parallel in training. some modules are on device cpu
+                    _input = torch.randn(_input_size)
+                    _input = _input.to(_device)
+                    _macs, _params = profile(_model, inputs=(_input, args.scale))
+                    _m_fm, _p_fm = clever_format([_macs, _params], "%.3f")#inputs=(_input, args.scale))
+                    _trop = str('\nThop: %s\'s total Parameters: %s(%d); MACs(Multi-Adds): %s(%d).\n' % (args.model, _p_fm, _params, _m_fm, _macs))
+                    # print(_trop)
+                    checkpoint.write_log(_trop)
+                    _input = _input.to(torch.device('cpu'))
                 if torch.cuda.is_available(): torch.cuda.empty_cache()  # release the cache costs on GPU by the statistics function
             t = Trainer(args, loader, _model, _loss, checkpoint, downmodel)
             while not t.terminate():
