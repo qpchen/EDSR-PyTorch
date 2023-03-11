b640a3d675e00cb0b6fd90cd2f2d982bf5e19a74
qpchen <chenqiangpu@foxmail.com>
2023-03-11 21:31:58 +0800

    delete redundent test

--- src/trainer.py
+++ src/trainer.py
@@ -304,8 +304,8 @@ class Trainer():
                                     #     sum_run_2 += runtime
                                 self.ckp.write_log('Average Runtime: {:.2f}ms.\n'.format((sum_run/_times)*1e3))
                                 # self.ckp.write_log('\nAverage Runtime except for first run: {:.2f}ms.'.format((sum_run_2/4)*1e3))
-                            else:
-                                sr = self.model(lr, idx_scale)
+                            # else:
+                            #     sr = self.model(lr, idx_scale)
                             sr = utility.quantize(sr, self.args.rgb_range)
 
                             save_list = [sr]
