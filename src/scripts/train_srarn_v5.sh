ad237d620812079d58fd56bbd0af5e77969b274c
qpchen <chenqiangpu@foxmail.com>
2023-03-28 13:36:28 +0800

    test git push by vscode

--- src/scripts/train_srarn_v5.sh
+++ src/scripts/train_srarn_v5.sh
@@ -12,8 +12,8 @@
 
 # run example for v5bn_x2: ./scripts/train_srarn_v5.sh train 0,1,2,3 1 bn b 2 48 ms 1acb3
 # run example for v5b_x2: ./scripts/train_srarn_v5.sh resume 0,1 1 b b 2 48 ms 1acb3
-# run example for v5b_Skip_x2: ./scripts/train_srarn_v5.sh train 2,3 1 b s 2 48 ms 1acb3              #bad#
-# run example for v5ba_1acb3_x2: ./scripts/train_srarn_v5.sh resume 2,3 1 ba b 2 48 ms 1acb3          #bad#
+# run example for v5b_Skip_x2: ./scripts/train_srarn_v5.sh train 2,3 1 b s 2 48 ms 1acb3       #bad#
+# run example for v5ba_1acb3_x2: ./scripts/train_srarn_v5.sh resume 2,3 1 ba b 2 48 ms 1acb3   #bad#
 # run example for v5ba_x2: ./scripts/train_srarn_v5.sh train 0,1 1 ba b 2 48 ms 1acb3
 # run example for v5bn_Skip_x2: ./scripts/train_srarn_v5.sh train 0,1 1 bn s 2 48 ms 1acb3
 
