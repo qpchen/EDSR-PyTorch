c0391c1c6258914d4c558e5c45393e51d4c353fa
qpchen <chenqiangpu@foxmail.com>
2023-07-21 12:05:54 +0800

    fix name in main

--- src/model/swinira.py
+++ src/model/swinira.py
@@ -939,7 +939,7 @@ if __name__ == '__main__':
     window_size = 8
     height = (1024 // upscale // window_size + 1) * window_size
     width = (720 // upscale // window_size + 1) * window_size
-    model = SwinIR(upscale=2, img_size=(height, width),
+    model = SwinIRA(upscale=2, img_size=(height, width),
                    window_size=window_size, img_range=1., depths=[6, 6, 6, 6],
                    embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2, upsampler='pixelshuffledirect')
     print(model)
